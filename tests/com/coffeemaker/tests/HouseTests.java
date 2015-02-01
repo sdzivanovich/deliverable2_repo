aac2608a876fefda1690524bbfc07ed5b774f20b
Sheridan Zivanovich <sdzivanovich@gmail.com>
2015-02-01 14:35:09 -0500

    Add more House unit tests, one is failing due to bug in House class.

--- tests/com/coffeemaker/tests/HouseTests.java
+++ tests/com/coffeemaker/tests/HouseTests.java
@@ -1,9 +1,12 @@
 package com.coffeemaker.tests;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
 
 import com.laboon.House;
+import com.laboon.Player;
 import com.laboon.Room;
 
 public class HouseTests 
@@ -61,7 +64,7 @@ public class HouseTests
                 hasCoffee = true;
             
             if (rooms[i].hasCream())
-                hasCoffee = true;
+                hasCream = true;
             
             if (rooms[i].hasSugar())
                 hasSugar = true;
@@ -70,6 +73,80 @@ public class HouseTests
         assertTrue(hasCoffee);
         assertTrue(hasSugar);
         assertTrue(hasCream);
+	}
+	
+	@Test
+	// Verify that the House will query the Room for items when
+	// the look method is called. 
+	public void houseQueriesRoomForItems()
+	{
+	    House subject = new House(5);
+	    
+	    Player player = new Player();
+	    Room room = mock(Room.class);
+	    
+	    when(room.hasItem()).thenReturn(true);
+	    when(room.hasCoffee()).thenReturn(false);
+	    when(room.hasSugar()).thenReturn(false);
+	    when(room.hasCream()).thenReturn(false);
+	    
+	    subject.look(player, room);
+	    
+	    verify(room).hasItem();
+	    verify(room).hasCoffee();
+	    verify(room).hasSugar();
+	    verify(room).hasCream();	    
+	}
+	
+	@Test
+	// Verify that the House will call the correspond getItem method
+	// on the player when looking at a room that contains the items.
+	public void playerGetsItemsFromRoomAfterLooking()
+	{
+	    House subject = new House(5);	   
+	    Player player = mock(Player.class);
+	    Room room = new Room(true, true, true, false, false);
+        
+        subject.look(player, room);
+        
+        verify(player).getCoffee();
+        verify(player).getSugar();
+        verify(player).getCream();
+	}
+	
+	@Test 
+	// Verify the getRoomDescription method returns the current room
+	// description.
+	public void getCurrentRoomInfoReturnsCurrentRoomDescription()
+	{
+	    Room room = mock(Room.class);
+	    
+	    String description = "a dummy description";
+	    when(room.getDescription()).thenReturn(description);
+	    
+	    Room[] rooms = { room };
+	    House subject = new House(rooms);
+	    
+	    assertEquals(description, subject.getCurrentRoomInfo());
+	}
+	
+	@Test 
+	// Verify that moveNorth when the current room has no North door
+	// does not change the current room. 
+	public void moveNorthDoesNotChangeRoomWhenRoomHasNoNorthernDoor()
+	{
+	    Room room = mock(Room.class);
+        
+        String description = "a dummy description";
+        when(room.getDescription()).thenReturn(description);
+        when(room.northExit()).thenReturn(false);
+        
+        Room[] rooms = { room };
+        House subject = new House(rooms);
+        
+        assertEquals(description, subject.getCurrentRoomInfo());
+        subject.moveNorth();
+        assertEquals(description, subject.getCurrentRoomInfo());
 	}
 
 }
